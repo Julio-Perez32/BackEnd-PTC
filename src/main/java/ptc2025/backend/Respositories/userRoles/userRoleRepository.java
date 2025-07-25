d7781a335ea3ce0a82f8e0efc34ca9a8ded5bd87
Julio-Perez32 <20210261@ricaldone.edu.sv>
2025-07-25 10:50:55 -0600

    commit

--- src/main/java/ptc2025/backend/Respositories/userRoles/userRoleRepository.java
+++
@@ -1,7 +0,0 @@
-package ptc2025.backend.Respositories.userRoles;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ptc2025.backend.Entities.userRoles.userRolesEntity;
-
-public interface userRoleRepository extends JpaRepository<userRolesEntity, String> {
-}
