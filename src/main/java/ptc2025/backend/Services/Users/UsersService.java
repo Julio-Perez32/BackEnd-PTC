5d9a939d11d1eaeb3638f68ef2b73a6d753e39b7
Iva <20240014@ricaldone.edu.sv>
2025-08-24 09:01:23 -0600

    ya esta la llave de systemRol en Users, completo

--- src/main/java/ptc2025/backend/Services/Users/UsersService.java
+++ src/main/java/ptc2025/backend/Services/Users/UsersService.java
@@ -7,9 +7,11 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import ptc2025.backend.Entities.Universities.UniversityEntity;
 import ptc2025.backend.Entities.Users.UsersEntity;
+import ptc2025.backend.Entities.systemRoles.SystemRolesEntity;
 import ptc2025.backend.Models.DTO.Users.UsersDTO;
 import ptc2025.backend.Respositories.Universities.UniversityRespository;
 import ptc2025.backend.Respositories.Users.UsersRespository;
+import ptc2025.backend.Respositories.systemRoles.systemRolesRespository;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,6 +25,9 @@ public class UsersService {
     @Autowired //Se inyecta el repositorio de University
     UniversityRespository repoUniversity;
 
+    @Autowired //Se inyecta el repositorio de University
+    systemRolesRespository repoSystemRoles;
+
     public List<UsersDTO> getAllUsers(){
         List<UsersEntity> users = repo.findAll();
         return users.stream()
@@ -34,16 +39,22 @@ public class UsersService {
         UsersDTO dto = new UsersDTO();
         dto.setId(usuario.getId());
         dto.setEmail(usuario.getEmail());
-        dto.setUsuario(usuario.getUsuario());
         dto.setContrasena(usuario.getContrasena());
-        dto.setFechaCreacion(usuario.getFechaCreacion());
-
+        //para convertir a DTO el id de la universidad del usuario
         if(usuario.getUniversity() != null){
             dto.setUniversityName(usuario.getUniversity().getUniversityName());
             dto.setUniversityID(usuario.getUniversity().getUniversityID());
         }else {
             dto.setUniversityName("Sin Universidad Asignada");
             dto.setUniversityID(null);
+        }
+        //para convertir a DTO el rol del sistema del usuario
+        if(usuario.getRoleId() != null){
+            dto.setRolesName(usuario.getSystemRoles().getRoleName());
+            dto.setRoleId(usuario.getSystemRoles().getRoleId());
+        }else {
+            dto.setRolesName("Sin Rol Asignado");
+            dto.setRoleId(null);
         }
         return dto;
     }
@@ -65,24 +76,41 @@ public class UsersService {
 
     private UsersEntity convertirAEntity(UsersDTO data){
         UsersEntity entity = new UsersEntity();
-        entity.setUniversityID(data.getUniversityID());
         entity.setEmail(data.getEmail());
-        entity.setUsuario(data.getUsuario());
         entity.setContrasena(data.getContrasena());
-        entity.setFechaCreacion(data.getFechaCreacion());
+        //Para obtener el id de universidad
+        if(data.getUniversityID() != null){
+            UniversityEntity university = repoUniversity.findById(data.getUniversityID())
+                    .orElseThrow(() -> new IllegalArgumentException("Universidad no encontrada con ID: " + data.getUniversityID()));
+            entity.setUniversity(university);
+        }
+        //para obtener el id rol del sistema
+        if(data.getRoleId() != null){
+            SystemRolesEntity systemRoles = repoSystemRoles.findById(data.getRoleId())
+                    .orElseThrow(() -> new IllegalArgumentException("Rol del sistema no encontrada con ID: " + data.getRoleId()));
+            entity.setSystemRoles(systemRoles);
+        }
         return entity;
     }
 
+    // Actualizar
     public UsersDTO actualizarDatos(String id, UsersDTO json) {
         UsersEntity existente = repo.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
         existente.setEmail(json.getEmail());
-        existente.setUsuario(json.getUsuario());
         existente.setContrasena(json.getContrasena());
-        existente.setFechaCreacion(json.getFechaCreacion());
+        //para obtener el id de la universidad
         if(json.getUniversityID() != null){
             UniversityEntity university = repoUniversity.findById(json.getUniversityID())
-                    .orElseThrow(() -> new IllegalArgumentException("Universidad no encontrada con ID: " + json.getUniversityID()));
+                    .orElseThrow(() -> new IllegalArgumentException("Universidad no encontrada con ID: " + json.getRoleId()));
             existente.setUniversity(university);
+        }else {
+            existente.setUniversity(null);
+        }
+        //para obtener el rol que tiene el usuario en el sistema
+        if(json.getRoleId() != null){
+            SystemRolesEntity systemRoles = repoSystemRoles.findById(json.getRoleId())
+                    .orElseThrow(() -> new IllegalArgumentException("Rol del sistema no encontrado con ID: " + json.getRoleId()));
+            existente.setSystemRoles(systemRoles);
         }else {
             existente.setUniversity(null);
         }
