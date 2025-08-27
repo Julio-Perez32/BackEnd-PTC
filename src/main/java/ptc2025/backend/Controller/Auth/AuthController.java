e06d0dfef7651b21463beda4c853dcd4348cf996
Iva <20240014@ricaldone.edu.sv>
2025-08-27 08:12:06 -0600

    Inicio de sesion basico

---
+++ src/main/java/ptc2025/backend/Controller/Auth/AuthController.java
@@ -0,0 +1,32 @@
+package ptc2025.backend.Controller.Auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ptc2025.backend.Models.DTO.Auth.LoginRequestDTO;
+import ptc2025.backend.Models.DTO.Auth.LoginResponseDTO;
+import ptc2025.backend.Services.AuthService.AuthService;
+
+@RestController
+@RequestMapping("/api/Auth")
+public class AuthController {
+    @Autowired
+    private AuthService service;
+
+    @PostMapping("/login")
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequest) {
+        try {
+            return service.login(
+                    loginRequest.getEmail(),
+                    loginRequest.getPassword()
+            );
+        } catch (Exception e) {
+            return new LoginResponseDTO(
+                    false,
+                    "Error en el servidor: " + e.getMessage(),
+                    null,
+                    null,
+                    null
+            );
+        }
+    }
+}
--- src/main/java/ptc2025/backend/Respositories/Users/UsersRespository.java
+++ src/main/java/ptc2025/backend/Respositories/Users/UsersRespository.java
@@ -1,7 +1,13 @@
 package ptc2025.backend.Respositories.Users;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ptc2025.backend.Entities.Users.UsersEntity;
 
+import java.util.Optional;
+
 public interface UsersRespository extends JpaRepository<UsersEntity, String> {
+    Optional<UsersEntity> findByEmail(String email);
 }
