f042072cb457f6bdda07f0165318c32fe6bc45be
Julio-Perez32 <20210261@ricaldone.edu.sv>
2025-09-15 16:00:25 -0600

    jeje  xd

---
+++ src/main/java/ptc2025/backend/Models/DTO/UserProfile/UserProfileDTO.java
@@ -0,0 +1,25 @@
+package ptc2025.backend.Models.DTO.UserProfile;
+
+import lombok.*;
+import ptc2025.backend.Entities.systemRoles.SystemRolesEntity;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfileDTO {
+    //datos users
+    private String userID;
+    private String email;
+    private String roleID;
+
+    //datos people
+    private String firstName;
+    private String lastName;
+    private Date birthdate;
+    private String contactEmail;
+    private String phone;
+
+}
