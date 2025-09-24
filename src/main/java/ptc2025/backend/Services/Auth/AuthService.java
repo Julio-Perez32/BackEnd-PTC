052961c1928e570ebe51f1d56be7478d11601c7c
Ivanya <ivanya@gmail.com>
2025-09-23 18:31:18 -0600

    lol oki

--- src/main/java/ptc2025/backend/Services/Auth/AuthService.java
+++ src/main/java/ptc2025/backend/Services/Auth/AuthService.java
@@ -1,5 +1,7 @@
 package ptc2025.backend.Services.Auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ptc2025.backend.Config.Argon2.Argon2Password;
@@ -9,6 +11,7 @@ import ptc2025.backend.Respositories.Users.UsersRespository;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class AuthService {
     @Autowired
@@ -19,14 +22,19 @@ public class AuthService {
         Argon2Password objHash = new Argon2Password();
         Optional<UsersEntity> List = repo.findByEmail(email).stream().findFirst();
         if(List.isPresent()){
+
             UsersEntity users = List.get();
+
+
             String userType = users.getSystemRoles().getRoleName();
             System.out.println("Usuario con el Id encontrado: " + users.getId()+
                     ", correo: "+ users.getEmail()+
                     ", rol: " + userType);
             return objHash.VerifyPassword(users.getContrasena(), password);
         }
+
         return false;
+
     }
     public Optional <UsersEntity> getUser(String email){
         Optional<UsersEntity> userOptional = repo.findByEmail(email);
