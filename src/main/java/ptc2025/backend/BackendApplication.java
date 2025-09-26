98bf993f245604b1b5937e01ab3a1b175cdb6bcc
Julio-Perez32 <20210261@ricaldone.edu.sv>
2025-09-26 14:24:18 -0600

    Deply 4

--- src/main/java/ptc2025/backend/BackendApplication.java
+++ src/main/java/ptc2025/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package ptc2025.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
