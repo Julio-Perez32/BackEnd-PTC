a504875368dae338fb017419d7d528e5d7d3fdf1
Julio-Perez32 <20210261@ricaldone.edu.sv>
2025-09-26 13:47:15 -0600

    Deply 1

--- src/main/java/ptc2025/backend/Config/Cloudinary/CloudinaryConfig.java
+++ src/main/java/ptc2025/backend/Config/Cloudinary/CloudinaryConfig.java
@@ -1,8 +1,6 @@
 package ptc2025.backend.Config.Cloudinary;
 
 import com.cloudinary.Cloudinary;
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,14 +16,11 @@ public class CloudinaryConfig {
 
     @Bean
     public Cloudinary cloudinary(){
-
-        Dotenv dotenv = Dotenv.load();
-
         Map<String, String> config = new HashMap<>();
 
-        config.put("cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"));
-        config.put("api_key", dotenv.get("CLOUDINARY_API_KEY"));
-        config.put("api_secret", dotenv.get("CLOUDINARY_API_SECRET"));
+        config.put("cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"));
+        config.put("api_key", System.getenv("CLOUDINARY_API_KEY"));
+        config.put("api_secret", System.getenv("CLOUDINARY_API_SECRET"));
 
         return new Cloudinary(config);
     }
